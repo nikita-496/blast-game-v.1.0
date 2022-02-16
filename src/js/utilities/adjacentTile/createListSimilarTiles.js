7f97e8ee064b1f0c9c123b68a3dbca8cea1b735b
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-02-16 09:29:02 +0500

    Edge case handling

--- src/js/utilities/adjacentTile/createListSimilarTiles.js
+++ src/js/utilities/adjacentTile/createListSimilarTiles.js
@@ -1,5 +1,15 @@
 const createListSimilarTiles = (currList, adjacentTiles) => {
-  let list = [currList, ...adjacentTiles];
+  let list = [];
+  if (!Array.isArray(currList)) {
+    list = [currList, ...adjacentTiles];
+  } else if (adjacentTiles.length === 0) {
+    list = currList;
+  } else {
+    for (let tile of adjacentTiles) {
+      list.push(tile);
+    }
+  }
+
   return list;
 };
 
